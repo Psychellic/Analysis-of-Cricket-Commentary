5cfa180185431a61e6bcd71512afe919109138b2
Psychellic <phychellic@gmail.com>
2024-05-10 22:39:05 +0530

    Trained the Model

--- Code/data_ext.py
+++ Code/data_ext.py
@@ -3,37 +3,41 @@ import os
 
 # Input and output directories
 input_dir = "../Data/COMMENTARY_INTL_MATCH"
-output_dir = "../Data/Commentary"
+output_dir = "../Data"
 
 # Create the output directory if it doesn't exist
 if not os.path.exists(output_dir):
-    os.makedirs(output_dir)  # Process each file in the input directory
+    os.makedirs(output_dir)
 
-for filename in os.listdir(input_dir):
-    if filename.endswith(".csv"):
-        input_file = os.path.join(input_dir, filename)
-        output_file = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}.csv")
+# Output file path
+output_file = os.path.join(output_dir, "Commentary_Entire.csv")
 
-        with open(input_file, "r", encoding="utf-8") as file_in, open(
-            output_file, "w", newline="", encoding="utf-8"
-        ) as file_out:
-            reader = csv.DictReader(file_in)
-            fieldnames = ["Commentary", "Runs", "Wickets"]
-            writer = csv.DictWriter(file_out, fieldnames=fieldnames)
-            writer.writeheader()
+# Open the output file in write mode
+with open(output_file, "w", newline="", encoding="utf-8") as file_out:
+    fieldnames = ["Commentary", "Runs", "Wickets"]
+    writer = csv.DictWriter(file_out, fieldnames=fieldnames)
+    writer.writeheader()
 
-            for row in reader:
-                commentary = row["Commentary"]
-                total_runs = row["Total_Runs_on_delivery"]
-                play_type_desc = row["PlayType_description"]
+    # Process each file in the input directory
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".csv"):
+            input_file = os.path.join(input_dir, filename)
 
-                # Check if the word "out" is present in the "PlayType_description" column
-                wickets = "1" if "out" in play_type_desc.lower() else "0"
+            with open(input_file, "r", encoding="utf-8") as file_in:
+                reader = csv.DictReader(file_in)
 
-                writer.writerow(
-                    {
-                        "Commentary": commentary,
-                        "Runs": total_runs,
-                        "Wickets": wickets,
-                    }
-                )
+                for row in reader:
+                    commentary = row["Commentary"]
+                    total_runs = row["Total_Runs_on_delivery"]
+                    play_type_desc = row["PlayType_description"]
+
+                    # Check if the word "out" is present in the "PlayType_description" column
+                    wickets = "1" if "out" in play_type_desc.lower() else "0"
+
+                    writer.writerow(
+                        {
+                            "Commentary": commentary,
+                            "Runs": total_runs,
+                            "Wickets": wickets,
+                        }
+                    )
--- Code/rnn.py
+++ Code/rnn.py
@@ -5,13 +5,13 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import LSTM, Dense, Embedding
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Embedding
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 
 # Load the dataset
-data = pd.read_csv("../Data/Commentary_1.csv")
+data = pd.read_csv("../Data/Commentary_Entire.csv")
 
 # Preprocess the data
 commentary = data["Commentary"].astype(str).values  # Convert to string type
@@ -35,8 +35,12 @@ X_train, X_test, y_runs_train, y_runs_test, y_wickets_train, y_wickets_test = (
 # Create the RNN model
 model = Sequential(
     [
-        Embedding(len(tokenizer.word_index) + 1, 100, input_length=max_length),
+        Embedding(len(tokenizer.word_index) + 1, 128, input_length=max_length),
+        LSTM(128, return_sequences=True),
+        Dropout(0.2),
         LSTM(64),
+        Dropout(0.2),
+        Dense(64, activation="relu"),
         Dense(32, activation="relu"),
         Dense(2, activation="linear"),
     ]
@@ -50,7 +54,7 @@ y_train = np.column_stack((y_runs_train, y_wickets_train))
 y_test = np.column_stack((y_runs_test, y_wickets_test))
 
 # Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=32)
+model.fit(X_train, y_train, epochs=20, batch_size=32)
 
 # Evaluate the model
 y_pred = model.predict(X_test)
@@ -59,20 +63,17 @@ wickets_pred = np.round(y_pred[:, 1]).astype(int)
 
 accuracy_runs = accuracy_score(y_runs_test, runs_pred)
 precision_runs = precision_score(y_runs_test, runs_pred, average="weighted")
-
 accuracy_wickets = accuracy_score(y_wickets_test, wickets_pred)
 precision_wickets = precision_score(y_wickets_test, wickets_pred, average="weighted")
 
 print("Runs Prediction:")
 print("Accuracy:", accuracy_runs)
 print("Precision:", precision_runs)
-
 print("Wickets Prediction:")
 print("Accuracy:", accuracy_wickets)
 print("Precision:", precision_wickets)
 
 output_dir = "../Code/Pickles"
-
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -84,9 +85,11 @@ with open("../Code/Pickles/trained_model.pkl", "wb") as file:
 commentary_input = "short and wide, poor ball. Warner goes back and slaps the ball through cover for four. Easy as ... Not much swing, just timed"
 commentary_sequence = tokenizer.texts_to_sequences([commentary_input])
 commentary_padded = pad_sequences(commentary_sequence, maxlen=max_length)
+
 prediction = model.predict(commentary_padded)
 predicted_runs = np.round(prediction[:, 0]).astype(int)[0]
 predicted_wickets = np.round(prediction[:, 1]).astype(int)[0]
+
 print("Predicted Runs:", predicted_runs)
 print("Predicted Wickets:", predicted_wickets)
 
@@ -94,8 +97,10 @@ print("Predicted Wickets:", predicted_wickets)
 commentary_input = "taken! Yasir Shah strikes. Flatter trajectory outside off, gets it to turn a touch but more importantly, gets some extra bounce. Smith sees something short and goes back to cut. The extra bounce is responsible for a thick outside edge. Sarfraz hangs onto a quality reflex catch, had his gloves in the right spot."
 commentary_sequence = tokenizer.texts_to_sequences([commentary_input])
 commentary_padded = pad_sequences(commentary_sequence, maxlen=max_length)
+
 prediction = model.predict(commentary_padded)
 predicted_runs = np.round(prediction[:, 0]).astype(int)[0]
 predicted_wickets = np.round(prediction[:, 1]).astype(int)[0]
+
 print("Predicted Runs:", predicted_runs)
 print("Predicted Wickets:", predicted_wickets)
